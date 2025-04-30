f80d8e649d340f9368305cfd36bf9e740915a591
MuriloHI <murilo.hidekiichioka@gmail.com>
2025-04-30 19:57:50 -0300

    Commit Atv 01 Relógio e Letreiro

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,10 +1,30 @@
-
+import { useState, useEffect } from 'react';
 
 export default function Letreiro() {
-    return (
-      <>
-        <h2>Meu Letreiro</h2>
-      </>
-    );
-  }
-    
+  const mensagemCompleta = 'Conheça a Fatec';
+  const [mensagemAtual, setMensagemAtual] = useState('');
+  const [letraAtual_index, setLetraAtual] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      if (letraAtual_index < mensagemCompleta.length) {
+        setMensagemAtual((textoAnterior) => textoAnterior + mensagemCompleta[letraAtual_index]);
+        setLetraAtual((indiceAnterior) => indiceAnterior + 1);
+      } else {
+        clearInterval(intervalo);
+        setTimeout(() => {
+          setMensagemAtual('');
+          setLetraAtual(0);
+        }, 1000);
+      }
+    }, 200);
+
+    return () => clearInterval(intervalo);
+  }, [letraAtual_index]);
+
+  return (
+    <section>
+      <h2>{mensagemAtual}</h2>
+    </section>
+  );
+}
--- src/Relogio.js
+++ src/Relogio.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react';
+
 export default function Relogio() {
-    return (
-      <>
-        <h2>Meu Relógio</h2>
-      </>
-    );
+
+  const [horaAtual, definirHoraAtual] = useState(new Date());
+
+  useEffect(() => {
+
+    const intervalo = setInterval(() => {
+      const novaHora = new Date();            
+      definirHoraAtual(novaHora);           
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalo);
+    }; 
+
+  }, []); 
+
+  const horas = horaAtual.getHours().toString().padStart(2, '0');
+  const minutos = horaAtual.getMinutes().toString().padStart(2, '0');
+  const segundos = horaAtual.getSeconds().toString().padStart(2, '0');
+
+  return (
+    <section>
+      <h1>{horas}:{minutos}:{segundos}</h1>
+    </section>
+  );
 }
